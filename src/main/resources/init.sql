469c9c95d1f61d38c813941216864c17429c153e
Hitesh Unnam <hitesh.unnam@ucdconnect.ie>
2024-07-03 21:28:40 +0100

    Implemented ability to add and host more than one website.

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -8,3 +8,12 @@ CREATE TABLE IF NOT EXISTS Messages (
     message TEXT NOT NULL,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS sites (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    domain VARCHAR(255) NOT NULL,
+    root VARCHAR(255) NOT NULL
+);
+
+INSERT INTO sites (domain, root) VALUES ('www.site1.com', '/var/www/site1');
+INSERT INTO sites (domain, root) VALUES ('www.site2.com', '/var/www/site2');
