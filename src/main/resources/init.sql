748b7020bf4f988c44b640d21deb4317972099a9
Hitesh Unnam <hitesh.unnam@ucdconnect.ie>
2024-07-07 11:18:36 +0100

    Fixed the docker-compose.yml and apache_config.conf files.

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -2,18 +2,11 @@ CREATE DATABASE IF NOT EXISTS FormData;
 
 USE FormData;
 
-CREATE TABLE IF NOT EXISTS Messages (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user VARCHAR(255) NOT NULL,
-    message TEXT NOT NULL,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
 CREATE TABLE IF NOT EXISTS sites (
     id INT PRIMARY KEY AUTO_INCREMENT,
     domain VARCHAR(255) NOT NULL,
     root VARCHAR(255) NOT NULL
 );
 
-INSERT INTO sites (domain, root) VALUES ('www.site1.com', '/WEB_ROOT/site1');
-INSERT INTO sites (domain, root) VALUES ('www.site2.com', '/WEB_ROOT/site2');
+INSERT INTO sites (domain, root) VALUES ('site1.local', '/WEB_ROOT/site1');
+INSERT INTO sites (domain, root) VALUES ('site2.local', '/WEB_ROOT/site2');
