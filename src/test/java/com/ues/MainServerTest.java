bf2acba9f2500b308cd87c743a544315936a9b12
ChungmanLee <51150076a@gmail.com>
2024-08-04 20:22:02 +0100

    Revert for Azure

--- src/test/java/com/ues/MainServerTest.java
+++ src/test/java/com/ues/MainServerTest.java
@@ -8,34 +8,34 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainServerTest {
 
-    // private Thread httpServerThread;
-    // private Thread secureServerThread;
+    private Thread httpServerThread;
+    private Thread secureServerThread;
 
-    // @BeforeEach
-    // void setUp() {
-    //     httpServerThread = new Thread(
-    //         new NioHttpServer()
-    //     );
+    @BeforeEach
+    void setUp() {
+        httpServerThread = new Thread(
+            new NioHttpServer()
+        );
 
-    //     secureServerThread = new Thread(
-    //         new SecureServer()
-    //     );
-    //     httpServerThread.start();
-    //     secureServerThread.start();
-    // }
+        secureServerThread = new Thread(
+            new SecureServer()
+        );
+        httpServerThread.start();
+        secureServerThread.start();
+    }
 
-    // @Test
-    // void testServerThreadsStarted() throws InterruptedException {
-    //     httpServerThread.join(5000);
-    //     secureServerThread.join(5000);
+    @Test
+    void testServerThreadsStarted() throws InterruptedException {
+        httpServerThread.join(5000);
+        secureServerThread.join(5000);
 
-    //     assertTrue(httpServerThread.isAlive(), "HTTP Server thread should be alive");
-    //     assertTrue(secureServerThread.isAlive(), "Secure Server thread should be alive");
-    // }
+        assertTrue(httpServerThread.isAlive(), "HTTP Server thread should be alive");
+        assertTrue(secureServerThread.isAlive(), "Secure Server thread should be alive");
+    }
 
-    // @AfterEach
-    // void tearDown() {
-    //     httpServerThread.interrupt();
-    //     secureServerThread.interrupt();
-    // }
+    @AfterEach
+    void tearDown() {
+        httpServerThread.interrupt();
+        secureServerThread.interrupt();
+    }
 }
